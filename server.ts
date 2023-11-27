45f2f75fe54e32fb3e6752b50000264a4f3fb8a6
tannazma <97254921+tannazma@users.noreply.github.com>
2023-11-27 19:14:20 +0100

    created express server

--- server.ts
+++ server.ts
@@ -1,1 +1,12 @@
-console.log("ughuh")
+import express from "express";
+ 
+const app = express();
+const port = 3001;
+ 
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+ 
+app.listen(port, () => {
+  console.log(`⚡ Server listening on port: ${port}`);
+});
