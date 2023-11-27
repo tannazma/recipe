c39c2e29ee4b389f10d1e6a94e69b81b1628128e
tannazma <97254921+tannazma@users.noreply.github.com>
2023-11-27 19:18:13 +0100

    created an endpoint for /recipes

--- server.ts
+++ server.ts
@@ -1,12 +1,20 @@
+import { PrismaClient } from "@prisma/client";
 import express from "express";
- 
+import recipes from "./prisma/data/recipes.json";
+
 const app = express();
 const port = 3001;
- 
+const prisma = new PrismaClient();
+
 app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
- 
+
+app.get("/recipes", async (req, res) => {
+  const allRecipes = await prisma.recipe.findMany({});
+  res.status(401).send(allRecipes);
+});
+
 app.listen(port, () => {
   console.log(`⚡ Server listening on port: ${port}`);
 });
