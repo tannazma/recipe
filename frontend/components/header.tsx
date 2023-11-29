25eadb5d5b8d9edf4ba75e73e17cca31fc451ea4
tannazma <97254921+tannazma@users.noreply.github.com>
2023-11-29 20:07:00 +0100

    created Header component which gets props to add many different image headers in many pages with different src

--- frontend/components/header.tsx
+++ frontend/components/header.tsx
@@ -1,8 +1,26 @@
-import background4 from "../data/bg-4.png"
-import React from 'react';
-import Image from 'next/image'
+import background4 from "../data/bg-4.png";
+import React from "react";
+import Image from "next/image";
 
-function Header() {
-    return <Image src="/bg-4.png" alt="Background4" width={1920} height={877.28} className="background4"/>;
-  }
-  export default Header
+interface HeadersProps {
+  src: string;
+  alt: string;
+  width: number;
+  height: number;
+  className: string;
+}
+
+function Header({ src, alt, width, height, className }: HeadersProps) {
+  return (
+    <Image
+      src={src}
+      alt={alt}
+      width={width}
+      height={height}
+      className={className}
+    />
+  );
+}
+export default Header;
+
+// <Image src="/bg-4.png" alt="Background4" width={1920} height={877.28} className="background4"/>;
