551696339d3bb070ac35bcebe4ffa15be4b2f819
tannazma <97254921+tannazma@users.noreply.github.com>
2023-12-01 12:52:48 +0100

    changed the defalt picture for recipeCard

--- frontend/components/recipeCard.tsx
+++ frontend/components/recipeCard.tsx
@@ -8,6 +8,15 @@
       <img
         src={
           recipe.img_url ||
+      <img
+        src={
+          recipe.img_url ||
+          "  https://joyfoodsunshine.com/wp-content/uploads/2022/06/chicken-kebabs-recipe-1.jpg"
+        }
+        className="recipe-image"
+      />
+        src={
+          recipe.img_url ||
           "  https://joyfoodsunshine.com/wp-content/uploads/2022/06/chicken-kebabs-recipe-1.jpg"
         }
         className="recipe-image"
