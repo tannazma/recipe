89b2e936b0bdc677169a05ab305399dff3b14eec
tannazma <97254921+tannazma@users.noreply.github.com>
2023-12-01 12:48:56 +0100

    changed the default picture in home for recipeCard

---
+++ frontend/components/recipeCard.tsx
@@ -0,0 +1,7 @@
+      <img
+        src={
+          recipe.img_url ||
+          "  https://joyfoodsunshine.com/wp-content/uploads/2022/06/chicken-kebabs-recipe-1.jpg"
+        }
+        className="recipe-image"
+      />
