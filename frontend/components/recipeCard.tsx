941eb58ada2e656771d694f2ddc4e71ba97a82cd
tannazma <97254921+tannazma@users.noreply.github.com>
2023-12-01 12:49:31 +0100

    chenged default image for recipeCard

--- frontend/components/recipeCard.tsx
+++ frontend/components/recipeCard.tsx
@@ -5,3 +5,10 @@
         }
         className="recipe-image"
       />
+      <img
+        src={
+          recipe.img_url ||
+          "  https://joyfoodsunshine.com/wp-content/uploads/2022/06/chicken-kebabs-recipe-1.jpg"
+        }
+        className="recipe-image"
+      />
