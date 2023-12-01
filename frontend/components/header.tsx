28a5c541f278088d2e2ccb3b5d5c566c52dca491
tannazma <97254921+tannazma@users.noreply.github.com>
2023-12-01 15:43:46 +0100

    remove unnecessary code

--- frontend/components/header.tsx
+++ frontend/components/header.tsx
@@ -1,4 +1,3 @@
-import background4 from "../data/bg-4.png";
 import React from "react";
 import Image from "next/image";
 
@@ -22,5 +21,3 @@ function Header({ src, alt, width, height, className }: HeadersProps) {
   );
 }
 export default Header;
-
-// <Image src="/bg-4.png" alt="Background4" width={1920} height={877.28} className="background4"/>;
