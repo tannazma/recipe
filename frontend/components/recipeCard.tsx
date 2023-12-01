895f69fcc83840da83be13eeb0ae33894d08bef5
tannazma <97254921+tannazma@users.noreply.github.com>
2023-12-01 13:01:59 +0100

    before changing recipeCard without rating stars

--- frontend/components/recipeCard.tsx
+++ frontend/components/recipeCard.tsx
@@ -6,46 +6,11 @@ interface RecipeProp {
 }
 
 const RecipeCard = ({ recipe }: RecipeProp) => {
-  // using reduce
-  let averageRating = 0;
-
-  // const averageRating =
-  //   recipe.comment.reduce((prev, curr) => prev + curr.rating, 0) /
-  //   recipe.comment.length;
-
-  // using complex method:
-  // Get the array of comments
-  const comments = recipe.comment;
-
-  // Check if comments is defined and is an array
-  if (comments && Array.isArray(comments) && comments.length > 0) {
-    // Calculate the total rating by summing up the rating of each comment
-    let totalRating = 0;
-    for (let i = 0; i < comments.length; i++) {
-      totalRating += comments[i].rating;
-    }
-
-    // Calculate the average rating by dividing the total rating by the number of comments
-    averageRating = totalRating / comments.length;
-  }
-
   return (
     <div key={recipe.id} className="recipe-card">
-      <img
-        src={
-          recipe.img_url ||
-          "  https://joyfoodsunshine.com/wp-content/uploads/2022/06/chicken-kebabs-recipe-1.jpg"
-        }
-        className="recipe-image"
-      />
+      <img src={recipe.img_url || 'https://placehold.co/310x320/'} className="recipe-image" />
       <div className="recipe-name">{recipe.name}</div>
-      <div className="rating">
-        {[1, 2, 3, 4, 5].map((num) => (
-          <span key={num} className="star-btn">
-            {num <= averageRating ? "★" : "☆"}
-          </span>
-        ))}
-      </div>
+      <div className="rating">rating</div>
     </div>
   );
 };
