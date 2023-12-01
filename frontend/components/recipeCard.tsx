80e2397d4a1437958b8bad14181e7809a0ddd956
tannazma <97254921+tannazma@users.noreply.github.com>
2023-12-01 12:54:07 +0100

    created average rating star for recipeCard

--- frontend/components/recipeCard.tsx
+++ frontend/components/recipeCard.tsx
@@ -1,3 +1,36 @@
+import React from "react";
+import { Recipe } from "../types";
+
+interface RecipeProp {
+  recipe: Recipe;
+}
+
+const RecipeCard = ({ recipe }: RecipeProp) => {
+  // using reduce
+  let averageRating = 0;
+
+  // const averageRating =
+  //   recipe.comment.reduce((prev, curr) => prev + curr.rating, 0) /
+  //   recipe.comment.length;
+
+  // using complex method:
+  // Get the array of comments
+  const comments = recipe.comment;
+
+  // Check if comments is defined and is an array
+  if (comments && Array.isArray(comments) && comments.length > 0) {
+    // Calculate the total rating by summing up the rating of each comment
+    let totalRating = 0;
+    for (let i = 0; i < comments.length; i++) {
+      totalRating += comments[i].rating;
+    }
+
+    // Calculate the average rating by dividing the total rating by the number of comments
+    averageRating = totalRating / comments.length;
+  }
+
+  return (
+    <div key={recipe.id} className="recipe-card">
       <img
         src={
           recipe.img_url ||
@@ -5,19 +38,15 @@
         }
         className="recipe-image"
       />
-      <img
-        src={
-          recipe.img_url ||
-      <img
-        src={
-          recipe.img_url ||
-          "  https://joyfoodsunshine.com/wp-content/uploads/2022/06/chicken-kebabs-recipe-1.jpg"
-        }
-        className="recipe-image"
-      />
-        src={
-          recipe.img_url ||
-          "  https://joyfoodsunshine.com/wp-content/uploads/2022/06/chicken-kebabs-recipe-1.jpg"
-        }
-        className="recipe-image"
-      />
+      <div className="recipe-name">{recipe.name}</div>
+      <div className="rating">
+        {[1, 2, 3, 4, 5].map((num) => (
+          <span key={num} className="star-btn">
+            {num <= averageRating ? "★" : "☆"}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
+};
+export default RecipeCard;
